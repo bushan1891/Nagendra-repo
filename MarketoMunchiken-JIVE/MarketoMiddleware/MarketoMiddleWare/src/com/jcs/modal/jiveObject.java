a1aa0ef0ab277606ffd4f358918238e647e9e063
bushan nagendra <bushan1891@gmail.com>
2016-04-03 17:56:12 -0700

    Initial push

---
+++ MarketoMunchiken-JIVE/MarketoMiddleware/MarketoMiddleWare/src/com/jcs/modal/jiveObject.java
@@ -0,0 +1,24 @@
+package com.jcs.modal;
+
+import java.net.URL;
+
+public class jiveObject {
+
+	URL webhook;
+	activity activity;
+	public URL getWebhook() {
+		return webhook;
+	}
+	public void setWebhook(URL webhook) {
+		this.webhook = webhook;
+	}
+	public activity getActivity() {
+		return activity;
+	}
+	public void setActivity(activity activity) {
+		this.activity = activity;
+	}
+	
+	
+	
+}
